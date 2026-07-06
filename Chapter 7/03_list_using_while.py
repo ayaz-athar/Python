578acb19d78863ae2ff7d3b4cc41a0e7807d5ab3
Ayaz Athar <ayaz.athar.44@gmail.com>
2026-07-06 10:32:01 +0530

    Add files via upload

---
+++ Chapter 7/03_list_using_while.py
@@ -0,0 +1,5 @@
+l = ["Ayaz" , False , "Faraz" , "verizox"]
+i = 0
+while i < len(l):
+    print(l[i])
+    i += 1
---
+++ Chapter 7/08_break_and_continue.py
@@ -0,0 +1,18 @@
+for i in range(100):
+    if i == 44:
+        break #exits the loop when i is 44
+    print(i)
+
+for i in range(100):
+    if i == 44:
+        continue #skips this iteration when i is 44
+    print(i)
+
+
+    #pass
+    for i in range(645):
+        pass
+i = 0
+while(i<45):
+    print(i)
+i += 1
