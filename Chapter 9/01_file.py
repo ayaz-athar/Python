c2799218c5f65b070244efc3e3ffc4c9c7305f05
Ayaz Athar <ayaz.athar.44@gmail.com>
2026-07-09 10:11:21 +0530

    Add files via upload

---
+++ Chapter 9/01_file.py
@@ -0,0 +1,8 @@
+'''a = "a very long string with emails"
+emails = []
+3 seconds '''
+
+with open("file.txt", "r") as f:
+    data = f.read()
+
+print(data)
---
+++ Chapter 9/03_more_file_functions.py
@@ -0,0 +1,5 @@
+with open("file.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    print(line, type(line))
